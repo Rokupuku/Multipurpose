4a38ac4938f847558f57faf63b1c39a2c8efc5fc
Rokupuku <rlacnd145@naver.com>
2025-03-30 22:40:07 +0900

    25/03/30
    수정

--- chapter01.java
+++ chapter01.java
@@ -2,11 +2,6 @@ public class chapter01 {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("im student");
-        System.out.println("im student");
-        System.out.println("im student");
-        System.out.println("im student");
-        System.out.println("im student");
-        System.out.println("im student11111111111");
+
     }
 }
