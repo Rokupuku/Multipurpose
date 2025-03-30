9bad8b69ac3406e9038271f24bad0d6e59002ef3
Rokupuku <rlacnd145@naver.com>
2025-03-30 22:11:23 +0900

    25/03/30 Add
    4개의 나는 학생이다 출력 문구 작성함

--- chapter01.java
+++ chapter01.java
@@ -3,5 +3,9 @@ public class chapter01 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("im student");
+        System.out.println("im student");
+        System.out.println("im student");
+        System.out.println("im student");
+        System.out.println("im student");
     }
 }
