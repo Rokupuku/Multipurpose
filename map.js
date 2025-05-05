c72a2ab19cb4c1e350aa9753ef99c6456ef09d0d
Rokupuku <rlacnd145@naver.com>
2025-05-06 04:38:10 +0900

    250506 지도 수정

--- map.js
+++ map.js
@@ -1,14 +1,10 @@
 window.onload = function () {
 
-    kakao.maps.load(function () {
-        alert("kakao.maps.load 시작");
+    let mapContainer = document.getElementById('map');
+    let mapOption = {
+        center: new kakao.maps.LatLng(37.420215, 127.126876),
+        level: 3
+    };
 
-        let mapContainer = document.getElementById('map');
-        let mapOption = {
-            center: new kakao.maps.LatLng(33.450701, 126.570667),
-            level: 3
-        };
-
-        let map = new kakao.maps.Map(mapContainer, mapOption);
-    });
+    let map = new kakao.maps.Map(mapContainer, mapOption);
 };
